eb3641d9374bd8fed18a813df674fa9f26ac73a5
zztt <zztt@example.com>
2024-06-07 00:01:28 +0800

    ...

--- DataStructuresAndAlgorithms/DataStructures/DataStructures.cpp
+++ DataStructuresAndAlgorithms/DataStructures/DataStructures.cpp
@@ -339,40 +339,78 @@ int main() {
 #include <string>
 #include "myExceptions.h"
 #include "chapter-10/sortedChain.h"
+#include "chapter-10/skipList.h"
 
 int main() {
-    sortedChain<int, int> z;
-    pair<int, int> p;
+    //sortedChain<int, int> z;
+    //pair<int, int> p;
 
-    p.first = 2; p.second = 10;
-    z.insert(p);
-    p.first = 10; p.second = 50;
-    z.insert(p);
-    p.first = 6; p.second = 30;
-    z.insert(p);
-    p.first = 8; p.second = 40;
-    z.insert(p);
-    p.first = 1; p.second = 5;
-    z.insert(p);
-    p.first = 12; p.second = 60;
-    z.insert(p);
+    //p.first = 2; p.second = 10;
+    //z.insert(p);
+    //p.first = 10; p.second = 50;
+    //z.insert(p);
+    //p.first = 6; p.second = 30;
+    //z.insert(p);
+    //p.first = 8; p.second = 40;
+    //z.insert(p);
+    //p.first = 1; p.second = 5;
+    //z.insert(p);
+    //p.first = 12; p.second = 60;
+    //z.insert(p);
 
-    cout << "The chain is " << z << endl;
-    cout << "Its size is " << z.size() << endl;
+    //cout << "The chain is " << z << endl;
+    //cout << "Its size is " << z.size() << endl;
 
-    // test find
-    cout << "Element associated with 1 is " << z.find(1)->second << endl;
-    cout << "Element associated with 6 is " << z.find(6)->second << endl;
-    cout << "Element associated with 12 is " << z.find(12)->second << endl;
+    //// test find
+    //cout << "Element associated with 1 is " << z.find(1)->second << endl;
+    //cout << "Element associated with 6 is " << z.find(6)->second << endl;
+    //cout << "Element associated with 12 is " << z.find(12)->second << endl;
 
-    // test erase
-    z.erase(1);
-    z.erase(2);
-    z.erase(6);
-    z.erase(12);
-    cout << "Deleted 1, 2, 6, 12" << endl;
-    cout << "The chain is " << z << endl;
-    cout << "Its size is " << z.size() << endl;
+    //// test erase
+    //z.erase(1);
+    //z.erase(2);
+    //z.erase(6);
+    //z.erase(12);
+    //cout << "Deleted 1, 2, 6, 12" << endl;
+    //cout << "The chain is " << z << endl;
+    //cout << "Its size is " << z.size() << endl;
+
+    //skipList<int, int> z(1000);
+    //pair<int, int> p;
+
+    //// test insert
+    //p.first = 2; p.second = 10;
+    //z.insert(p);
+    //p.first = 10; p.second = 50;
+    //z.insert(p);
+    //p.first = 6; p.second = 30;
+    //z.insert(p);
+    //p.first = 8; p.second = 40;
+    //z.insert(p);
+    //p.first = 1; p.second = 5;
+    //z.insert(p);
+    //p.first = 12; p.second = 60;
+    //z.insert(p);
+    //cout << "The dictionary is " << z << endl;
+    //cout << "Its size is " << z.size() << endl;
+
+    //// test find
+    //cout << "Element associated with 1 is " << z.find(1)->second << endl;
+    //cout << "Element associated with 6 is " << z.find(6)->second << endl;
+    //cout << "Element associated with 12 is " << z.find(12)->second << endl;
+
+    //// test erase
+    //z.erase(1);
+    //z.erase(2);
+    //z.erase(6);
+    //z.erase(12);
+    //cout << "Deleted 1, 2, 6, 12" << endl;
+    //cout << "The dictionary is " << z << endl;
+    //cout << "Its size is " << z.size() << endl;
+
+
+
+
 
     exit(0);
 }
--- DataStructuresAndAlgorithms/DataStructures/chapter-10/skipList.h
+++ DataStructuresAndAlgorithms/DataStructures/chapter-10/skipList.h
@@ -20,7 +20,7 @@ public:
     bool empty() const { return dSize == 0; }
     int size() const { return dSize; }
     pair<const K, E>* find(const K&) const;
-    //void erase(const K&);
+    void erase(const K&);
     void insert(const pair<const K, E>&);
     void output(ostream& out) const;
 
@@ -41,7 +41,7 @@ protected:
 
 template<typename K, typename E>
 inline skipList<K, E>::skipList(K largeKey, int maxPairs, float prob)
-{
+{//关键字小于largeKey,字典最大预期数，0<prod<1
     cutOff = prob * RAND_MAX;
     maxLevel = (int)ceil(logf((float)maxPairs) / logf(1 / prob)) - 1;
     levels = 0;  // initial number of levels
@@ -55,7 +55,7 @@ inline skipList<K, E>::skipList(K largeKey, int maxPairs, float prob)
     tailNode = new skipNode<K, E>(tailPair, 0);
     last = new skipNode<K, E>* [maxLevel + 1];
 
-    // header points to tail at all levels as lists are empty
+    // 链表为空时，任意链表中的头节点都指向尾节点
     for (int i = 0; i <= maxLevel; i++)
         headerNode->next[i] = tailNode;
 }
@@ -81,45 +81,113 @@ template<typename K, typename E>
 inline pair<const K, E>* skipList<K, E>::find(const K& theKey) const
 {
     if (theKey >= tailKey)
-        return NULL;  // no matching pair possible
+        return NULL;  // 没有可能的匹配数对
 
-    // position beforeNode just before possible node with theKey
+    // 位置before 是关键字为theKey的节点之前最右边的位置
     skipNode<K, E>* beforeNode = headerNode;
-    for (int i = levels; i >= 0; i--)          // go down levels
-        // follow level i pointers
+    for (int i = levels; i >= 0; i--)          // 从上级链表到下级链表
         while (beforeNode->next[i]->element.first < theKey)
             beforeNode = beforeNode->next[i];
 
-    // check if next node has theKey
+    // 检查下一个节点的关键字是否是thekey
     if (beforeNode->next[0]->element.first == theKey)
         return &beforeNode->next[0]->element;
 
     return NULL;  // no matching pair
 }
 
-//template<typename K, typename E>
-//inline void skipList<K, E>::erase(const K&)
-//{
-//}
+template<typename K, typename E>
+inline void skipList<K, E>::erase(const K& theKey)
+{
+    if (theKey>=tailKey) {
+        return;
+    }
+
+    skipNode<K, E>* theNode = search(theKey);
+    if (theNode->element.first!=theKey) {
+        return;
+    }
+
+    for (int i = 0; i < levels && last[i]->next[i] == theNode;i++) {
+        last[i]->next[i] = theNode->next[i];
+    }
+    while (levels>0&&headerNode->next[levels]==tailNode) {
+        levels--;
+        delete theNode;
+        dSize--;
+    }
+}
 
 template<typename K, typename E>
-inline void skipList<K, E>::insert(const pair<const K, E>&)
+inline void skipList<K, E>::insert(const pair<const K, E>& thePair)
 {
+    //把数对thePair 插入
+    if (thePair.first>=tailKey) {
+        ostringstream s;
+        s << "Key = " << thePair.first << "Must be <" << tailKey;
+        throw illegalParameterValue(s.str());
+    }
+    //查看关键字为theKey的数对是否存在
+    skipNode<K, E>* theNode = search(thePair.first);
+    if (theNode->element.first == thePair.first) {
+        theNode->element.second = thePair.second;
+        return;
+    }
+    int theLevel = level();
+    if (theLevel>levels) {
+        theLevel = ++levels;
+        last[theLevel] = headerNode;
+    }
+
+    skipNode<K, E>* newNode = new skipNode<K, E>(thePair,theLevel+1);
+    for (int i = 0; i <= theLevel;i++) {
+        newNode->next[i] = last[i]->next[i];
+        last[i]->next[i] = newNode;
+    }
+    dSize++;
+    return;
 }
 
 template<typename K, typename E>
 inline void skipList<K, E>::output(ostream& out) const
 {
+    for (skipNode<K, E>* currentNode = headerNode->next[0];
+        currentNode != tailNode;
+        currentNode = currentNode->next[0])
+        out << currentNode->element.first << " "
+        << currentNode->element.second << "  ";
+}
+
+template <class K, class E>
+ostream& operator<<(ostream& out, const skipList<K, E>& x)
+{
+    x.output(out); return out;
 }
 
 template<typename K, typename E>
 inline int skipList<K, E>::level() const
 {
-    return 0;
+    //返回一个随机层数，这个数不大于maxLevel
+    int lev = 0;
+    while (rand()<=cutOff)
+    {
+        lev++;
+    }
+    return (lev <= maxLevel) ? lev : maxLevel;
 }
 
 template<typename K, typename E>
-inline skipNode<K, E>* skipList<K, E>::search(const K&) const
+inline skipNode<K, E>* skipList<K, E>::search(const K& theKey) const
 {
-    return nullptr;
+    //搜索关键字theKey，把每一级链表中要查看的最后一节存储在数组last中
+    //返回包含关键词theKey的节点
+    skipNode<K, E>* beforeNode = headerNode;
+    for (int i = levels; i >= 0;i--) {
+        while (beforeNode->next[i]->element.first<theKey)
+        {
+            beforeNode = beforeNode->next[i];
+        }
+        last[i] = beforeNode;
+    }
+    return beforeNode->next[0];
 }
